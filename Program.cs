33b8cb21b15610eb808fd41b11bde2d67c30341b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-29 22:38:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.ForegroundColor = ConsoleColor.White;
+System.Console.WriteLine("---- Um Dois Três Pim ----");
+Console.ResetColor();
+
+for (int n = 1; n <= 40; n++)
+ if ( n % 4 == 0)
+ {
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("pim\n");
+    Console.ResetColor();
+ }
+
+ else
+ {
+    Console.ForegroundColor = ConsoleColor.White;
+    System.Console.Write($"{n,2} ");
+    Console.ResetColor();
+ }
